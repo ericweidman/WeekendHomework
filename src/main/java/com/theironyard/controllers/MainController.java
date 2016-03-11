a6f24204ea1858508df53b8c4eaf53119ba0db13
ericweidman <ericweidman@gmail.com>
2016-03-11 15:29:59 -0500

    Added stub routes, edited home.html

--- src/main/java/com/theironyard/controllers/MainController.java
+++ src/main/java/com/theironyard/controllers/MainController.java
@@ -1,6 +1,5 @@
 package com.theironyard.controllers;
 
-import com.theironyard.entities.Job;
 import com.theironyard.entities.User;
 import com.theironyard.services.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import javax.servlet.http.HttpSession;
+
 
 /**
  * Created by ericweidman on 3/10/16.
@@ -37,4 +36,22 @@ public class MainController {
         session.setAttribute("userName", userName);
         return "redirect:/";
     }
+
+    @RequestMapping(path = "/logout", method = RequestMethod.POST)
+    public  String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    public String add(HttpSession session){
+
+        return "redirect:/";
+    }
+    @RequestMapping(path = "/remove", method = RequestMethod.POST)
+    public String remove(HttpSession session){
+
+        return "redirect:/";
+    }
+
 }
